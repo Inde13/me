bb852df5e2a2877674978b9a6fea6c88ea37e965
Matheus Faria Hanzoi <mahfariah13@gmail.com>
2023-05-21 17:24:08 -0300

    Name input System

--- specialusers.js
+++ specialusers.js
@@ -1,10 +1,13 @@
-document.getElementById("name-input").style.visibility = 'hidden'
 var username = ""
-
-function insertName() {
-    document.getElementById("name-input").style.visibility = 'visible'
-}
 
 function setName() {
     username = document.getElementById('name-input').value
+    document.getElementById('name-input').value = ""
+}
+
+function checkName() {
+    if (username == "Teste") {
+        window.location.href = "index.html"
+        document.getElementById("eu-sou").innerHTML = "Eu sou Teste"
+    }
 }
