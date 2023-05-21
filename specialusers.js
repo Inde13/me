3b24716150394d17c3f296f558264f72afd9ee51
Matheus Faria Hanzoi <mahfariah13@gmail.com>
2023-05-20 22:21:15 -0300

    centralization and name input

--- specialusers.js
+++ specialusers.js
@@ -1,1 +1,10 @@
 document.getElementById("name-input").style.visibility = 'hidden'
+var username = ""
+
+function insertName() {
+    document.getElementById("name-input").style.visibility = 'visible'
+}
+
+function setName() {
+    username = document.getElementById('name-input').value
+}
